06e60c25b323d776a110e6545c0882d5ef723222
Ariel Khais <arielkhais@hotmail.com>
2025-06-03 20:49:57 -0700

    my changes

--- lua/options.lua
+++ lua/options.lua
@@ -61,4 +61,10 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.filetype.add {
+  extension = {
+    ksy = 'yaml',
+  },
+}
+
 -- vim: ts=2 sts=2 sw=2 et
